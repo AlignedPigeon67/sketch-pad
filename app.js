94a6b15cc1aebe3aba12bd8dbc31dc08bda01b44
tomhine <tomhine792@user.github.com>
2020-12-09 22:14:05 +0000

    Created grid and mous event to change element colour on mouse over

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const pad = document.querySelector("#pad");
-let gridSize = 16;
+let gridDimesion = 8;
+
+function initGrid(dim) {
+    for (let i = 0; i < dim; i++) {
+        for (let j = 0; j < dim; j++) {
+            let newDiv = document.createElement("div");
+            newDiv.addEventListener("mouseover", () => {
+                newDiv.classList.add("draw-div")
+            })
+            pad.appendChild(newDiv);
+        }
+    }
+}
+
+initGrid(gridDimesion);
