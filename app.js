95c5c49cc4a7320db247b10e00e493e7ef775ffc
tomhine <tomhine792@user.github.com>
2020-12-09 23:25:42 +0000

    fixed grid issue with div overlapping pad area, changed main grid to stop vertical overflow

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const pad = document.querySelector("#pad");
-let gridDimesion = 8;
+let gridDimesion = 16;
 
 function initGrid(dim) {
     for (let i = 0; i < dim; i++) {
         for (let j = 0; j < dim; j++) {
             let newDiv = document.createElement("div");
             newDiv.addEventListener("mouseover", () => {
-                newDiv.classList.add("draw-div")
-            })
+                newDiv.classList.add("draw-div");
+            });
             pad.appendChild(newDiv);
         }
     }
